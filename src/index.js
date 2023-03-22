c31e54ceac1c578e415d5d46ef23189842a45cea
livi <scholtz.ice@gmail.com>
2023-03-22 14:33:33 +0200

    Cleaned up file structure

--- src/index.js
+++ src/index.js
@@ -4,24 +4,3 @@ const routes = {
     "/services": "services",
     "/contact": "contact",
 };
-
-const router = new Router(routes);
-
-// import { Router } from "./router.js";
-
-// const router = new Router();
-
-// router.addRoute("/", () => {
-//     // Code to render the home page
-// });
-
-// router.addRoute("/about", () => {
-//     // Code to render the about page
-// });
-
-// router.addRoute("/contact", () => {
-//     // Code to render the contact page
-// });
-
-// // Start the router
-// router.init();
