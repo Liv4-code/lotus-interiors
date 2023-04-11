037e71e24ac1a4c9558af9fe0dec87174e154962
livi <scholtz.ice@gmail.com>
2023-04-11 17:47:36 +0200

    Set event for dropdown to be removed when viewport is resized

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 const navDownArrow = document.querySelector(".navbar__down-arrow");
+const navDropdown = document.querySelector(".navbar__dropdown");
+const viewPort = window.visualViewport;
 
+// Displaying or hiding dropdown when arrow is clicked
 navDownArrow.addEventListener("click", () => {
-    const navDropdown = document.querySelector(".navbar__dropdown");
-    console.log(navDropdown.childNodes);
-
     if (navDropdown.style.display === "flex") {
         navDropdown.style.display = "none";
     } else {
         navDropdown.style.display = "flex";
     }
 });
+
+// Removing dropdown when viewport width is resized to tablet or larger
+viewPort.addEventListener("resize", () => {
+    if (window.visualViewport.width >= 768) {
+        console.log("Wah!");
+        navDropdown.style.display = "none";
+    }
+});
+
+// Removing dropdown if viewport width is size of tablet or larger
+if (viewPort.width >= 768) {
+    console.log("Wah!");
+    navDropdown.style.display = "none";
+}
